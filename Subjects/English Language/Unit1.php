48129f28572cd82211b0ce19945ddefae4c51f8a
mattjenson- <mattyj1707@gmail.com>
2015-01-03 18:28:13 +0000

    Started adding content

--- Subjects/English Language/Unit1.php
+++ Subjects/English Language/Unit1.php
@@ -35,12 +35,32 @@
 
   <section class="Questions">
  <h3> General Overview</h3>
-  <p> </p> 
+  <p> The Unit 1 exam paper counts for 60% of your total GCSE, it is essential that you properly prepare for it. 
+There are different approaches you can take to tackle the paper, however if you manage your time correctly then 
+you will be able to complete everything in the time given no matter what order you do the questions in. Timing is
+everything, if you have troubles with eyesight and cannot see the clock in the exam room make sure you are wearing 
+a watch! </p> 
  <h3> Question 1</h3>
+ <p><i>8 marks - 3 minutes active reading, 12 minutes writing.</i></p>
+ <p> The first question on the paper will always ask you to talk about <b>source one</b> in the booklet. You will 
+ have to summarise and show your understanding holistically across the whole of the text. Make sure you actively read
+ through the whole text before you start writing, highlighting the key points that the question is asking for as you 
+ go. You should make 4-5 points chronologically through the text, using short embedded quotations to back the points
+ up. Try to think outside of the box with your interpretations, make them interesting and perceptive; use inference. 
+ This is one of the easiest questions on the paper, just because you don't think it's hard it doesn't mean it needs 
+ care when writing! </p>
  <h3> Question 2</h3>
+ <p><i>8 marks - 3 minutes active reading, 12 minutes writing.</i></p>
+ <p> The second question will always ask you to analyse the effect of the (sub)headline and the picture in <b>source
+ two</b> and link these to specific points in the text. The best way to tackle this question is HEL, PEL, HEL, PEL. 
+ These are each paragraphs where you talk about the <b>H</b>eadline or <b>P</b>icture, how they are <b>E</b>ffective,
+ and how they <b>L</b>ink to the text.  </p>
  <h3> Question 3</h3>
+ <p><i>8 marks - 3 minutes active reading, 12 minutes writing.</i></p>
  <h3> Question 4</h3>
+ <p><i>16 marks - 6 minutes active reading, 24 minutes writing.</i></p>
  <h3> Question 5</h3>
+ <p><i>16 marks - 5 minutes planning, 20 minutes writing.</i></p>
  <h3> Question 6</h3>
-
+ <p><i>24 marks - 5 minutes planning, 30 minutes writing.</i></p>
 </section>
