cc2a9c8dc84b074c66183dc232750931f58834f0
mattjenson- <mattyj1707@gmail.com>
2015-01-03 18:07:07 +0000

    Test to see if it will work...

--- Subjects/English Language/Unit1.php
+++ Subjects/English Language/Unit1.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 
 <head>
-  <title> Unit 1 </title>
+  <title> English Language: Unit 1 </title>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400;300' rel='stylesheet' type='text/css'>
-  <link href='file://C:\Users\Nathan\Documents\GitHub\GCSErevision\style.css' rel='stylesheet'>
-  <link rel="stylesheet" href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\assets/base.css">
-  <link rel="stylesheet" href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\fixed-navigation-bar.css">
-  <link rel="shortcut icon" href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\icon.ico">
+  <link href='C:\Users\Matthew\Documents\GitHub\GCSErevision\style.css' rel='stylesheet'>
+  <link rel="stylesheet" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\assets/base.css">
+  <link rel="stylesheet" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\fixed-navigation-bar.css">
+  <link rel="shortcut icon" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\icon.ico">
 </head>
 
 <!--Nav Bar-->
 <nav class="fixed-nav-bar">
   <div id="menu" class="menu">
-    <a class="sitename" href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\index.php">GCSE
+    <a class="sitename" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\index.php">GCSE
     </a>
     <!-- Example responsive navigation menu  -->
     <a class="show" href="#menu">Menu</a><a class="hide" href="#hidemenu">Menu</a>
     <ul class="menu-items">
-      <li><a href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\subjects/sciences/science.php">Science</a></li>
-      <li><a href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\subjects/geography/casestudy.php">Case Studys</a></li>
-      <li><a href="file://C:\Users\Nathan\Documents\GitHub\GCSErevision\subjects/english Language/unit1.php">English Unit 1</a></li>
+      <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/sciences/science.php">Science</a></li>
+      <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/geography/casestudy.php">Case Studys</a></li>
+      <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/english Language/unit1.php">English Unit 1</a></li>
     </ul>
   </div>
 </nav>
@@ -34,6 +34,8 @@
   </section>
 
   <section class="Questions">
+ <h3> General Overview</h3>
+  <p> </p> 
  <h3> Question 1</h3>
  <h3> Question 2</h3>
  <h3> Question 3</h3>
