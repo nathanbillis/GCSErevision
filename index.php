9b4c733fd73607aef69c31abeb96686a0ba8c799
Nathan Billis <nathanbillis.nathan@gmail.com>
2015-12-24 11:24:19 +0000

    Changed parts in VS15
    Changed parts of the project iv visual studio 2015

--- index.php
+++ index.php
@@ -3,24 +3,24 @@
 <head>
   <title> Welcome To GCSE Revision </title>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400;300' rel='stylesheet' type='text/css'>
-  <link href='file://C:\Users\Matthew\Documents\GitHub\GCSErevision\style.css' rel='stylesheet'>
-  <link rel="stylesheet" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\assets/base.css">
-  <link rel="stylesheet" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\fixed-navigation-bar.css">
-  <link rel="shortcut icon" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\icon.ico">
+  <link href='/style.css' rel='stylesheet'>
+  <link rel="stylesheet" href="/assets/base.css">
+  <link rel="stylesheet" href="/fixed-navigation-bar.css">
+  <link rel="shortcut icon" href="/icon.ico">
 </head>
 
   <body>
 <!--Nav Bar-->
     <nav class="fixed-nav-bar">
     <div id="menu" class="menu">
-      <a class="sitename" href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\index.php">GCSE
+      <a class="sitename" href="/GCSErevision\index.php">GCSE
       </a>
       <!-- Example responsive navigation menu  -->
       <a class="show" href="#menu">Menu</a><a class="hide" href="#hidemenu">Menu</a>
       <ul class="menu-items">
-        <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/sciences/science.php">Science</a></li>
-        <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/geography/casestudy.php">Case Studys</a></li>
-        <li><a href="file://C:\Users\Matthew\Documents\GitHub\GCSErevision\subjects/english Language/unit1.php">English Unit 1</a></li>
+        <li><a href="/subjects/sciences/science.php">Science</a></li>
+        <li><a href="/subjects/geography/casestudy.php">Case Studys</a></li>
+        <li><a href="/subjects/english Language/unit1.php">English Unit 1</a></li>
               </ul>
             </div>
           </nav>
